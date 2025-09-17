773a910c5bce1eab9be3a9cd67b1e579b9c8a8d4
RafaMartins87 <rafael.martins87@outlook.com>
2025-09-16 22:32:22 -0300

    ajuste

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/martech/',
+  base: '/marktech/',
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
